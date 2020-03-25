338106233cee066f9a5c96c138929d76fafd592d
Nathaniel Redmon <52013906+nonstopper0@users.noreply.github.com>
2020-03-25 13:59:07 -0600

    added pictures for garage aswell as garage page finalized with slide functionality

--- src/Team.js
+++ src/Team.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import GarageDoor from './garage/garagedoor2.png'
 import Garage from './garage/garagedoor.png'
+import { Slide } from 'react-slideshow-image';
 
 export default class Team extends React.Component {
     render() {
@@ -10,11 +11,32 @@ export default class Team extends React.Component {
                     <img id="garage" style={styles.garage} src={Garage}></img>
                     <div style={styles.doorContainer} >
                         <img id="garageDoor" style={styles.garageDoor} src={GarageDoor}></img>
-                    </div>
+                        <div className="slide-container">
+                            <Slide {...slideProperties}>
+                                <div className="each-slide">
+                                    <div style={{'backgroundImage': ``}}>
+                                    </div>
+                                </div>
+                                <div className="each-slide">
+                                    <div style={{'backgroundImage': ''}}>
+                                    </div>
+                                </div>
+                            </Slide>
+                        </div>
+                    </div>
                 </div>
             </React.Fragment>
         )
     }
+}
+
+const slideProperties = {
+    duration: 5000,
+    transitionDuration: 1000,
+    infinite: true,
+    indicators: true,
+    arrows: true,
+    pauseOnHover: true,
 }
 
 const styles = {
@@ -26,21 +48,21 @@ const styles = {
     },
     doorContainer: {
         position: 'relative',
-        top: 200,
+        top: 260,
         overflow: 'hidden',
-        height: '700px',
-        width: '870px',
-        left: 55,
+        height: '645px',
+        width: '860px',
+        left: 65,
     },
     garage: {
         zIndex: 1,
         position: 'absolute'
     },
     garageDoor: {
-        left: -55,
+        left: -64,
         opacity: 0,
-        top: -200,
-        zIndex: 0,
+        top: -260,
+        zIndex: 1,
         position: 'absolute'
     }
 }
